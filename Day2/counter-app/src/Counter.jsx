102e6236c5225d95afdb5d6786571ffdcdde7c97
prasanna <prasanna1@gmail.com>
2025-12-18 23:09:17 +0530

    initial setup done

---
+++ Day2/counter-app/src/Counter.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count !== 0 && count % 3 === 0) {
+      alert(`The current number ${count} is divisible by 3`);
+    }
+  }, [count]); // runs ONLY when count changes
+
+  return (
+    <div>
+      <h2>Counter App</h2>
+      <h3>Count: {count}</h3>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+    </div>
+  );
+}
+
+export default Counter;
+
